4e291f3776761bdf44569cf63e3a4eb2fc17a4a1
richard <richardroygibson@gmail.com>
2021-01-24 17:28:20 +1100

    first commit

---
+++ sketch.js
@@ -0,0 +1,19 @@
+const flock = []
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+	for(let i=0; i<100; i++){
+	flock.push(new Boid())
+	}
+}
+
+function draw() {
+	background(51);
+
+	for(const boid of flock) {
+		boid.edges();
+		boid.flock(flock);
+		boid.update();
+		boid.show();
+	}
+}
