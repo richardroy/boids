80063d53c6563f3e8155c14af5c72f2b69202d8e
richard <richardroygibson@gmail.com>
2021-01-26 11:28:40 +1100

    requires a heap of work but a slow start on reflecting on a wall

--- boid.js
+++ boid.js
@@ -7,12 +7,91 @@ class Boid {
 		this.acceleration = createVector();
 		this.maxForce = 0.1;
 		this.maxSpeed = 5;
-		this.perception = 50;
+		this.perception = 100;
 
 		this.wanderAngle = random()*PI*2;;
 	}
 
 	edges() {
+		
+		const leftEdge1 = createVector(20, 20);
+		const leftEdge2 = createVector(20, height-20);
+
+		const topEdge1 = createVector(20, 20);
+		const topEdge2 = createVector(width-20, 20);
+
+		const rightEdge1 = createVector(width-20, height-20);
+		const rightEdge2 = createVector(width-20, 20);
+
+		const bottomEdge1 = createVector(width-20, height-20);
+		const bottomEdge2 = createVector(20, height-20);
+
+		const edges = [
+			{
+				top: leftEdge1,
+				bottom: leftEdge2
+			},
+			{
+				top: topEdge1,
+				bottom: topEdge2
+			},
+			{
+				top: rightEdge1,
+				bottom: rightEdge2
+			},
+			{
+				top: bottomEdge1,
+				bottom: bottomEdge2
+			}
+		]
+
+		let b = createVector();
+
+		if (this.id == 0) {
+			for(let edge of edges) {
+				let hit = collideLineLine(edge.top.x, edge.top.y, edge.bottom.x, edge.bottom.y, this.position.x, this.position.y, this.position.x + this.velocity.copy().normalize().x * 50, this.position.y + this.velocity.copy().normalize().y * 50, true);
+				const hitX = hit.x >= 0 ? hit.x-50 : hit.x+50; 
+				const hitY = hit.y >= 0 ? hit.y-50 : hit.y+50; 
+
+				let hitVector = createVector(hit.x, hit.y);
+				if(hit.x !== false && hit.y !== false) {
+					//show collision point
+					circle(hitVector.x, hitVector.y, 10)
+					
+
+					//calc normal of edge
+					let dx = edge.bottom.x - edge.top.x;
+					let dy = edge.bottom.y - edge.top.y;
+
+					//if dy =
+
+					// line(hitVector.x, hitVector.y, dy, -dx)
+					line(dy, -dx, -dy, dx)
+					let a = createVector();
+					if(dy == 0)
+						a.add(-dy, dx).normalize().mult(25);
+					else if (dx == 0)
+						a.add(dy, -dx).normalize().mult(25);
+					
+					// normal line
+					line(hitVector.x, hitVector.y, hitVector.x + a.x, hitVector.y + a.y)
+					b = this.velocity.copy().add(this.position).reflect(a);
+					console.log(a)
+					line(hitVector.x, hitVector.y, hitVector.x + b.x, hitVector.y + b.y)
+
+				}
+			}
+		}
+
+		// console.log(edges)
+
+
+		// line(20, 20, 20, height - 20);
+		line(leftEdge1.x, leftEdge1.y, leftEdge2.x, leftEdge2.y);
+		line(topEdge1.x, topEdge1.y, topEdge2.x, topEdge2.y);
+		line(rightEdge1.x, rightEdge1.y, rightEdge2.x, rightEdge2.y);
+		line(bottomEdge1.x, bottomEdge1.y, bottomEdge2.x, bottomEdge2.y);
+
 		if(this.position.x > width + 20)
 			this.position.x = -20;
 		else if (this.position.x < -20)
@@ -22,6 +101,8 @@ class Boid {
 			this.position.y = -20
 		else if (this.position.y < -20)
 			this.position.y = height + 20
+
+		return b;
 	}
 
 	align(boids) {
@@ -103,11 +184,13 @@ class Boid {
 		let cohesion = this.cohesion(boids);
 		let seperation = this.seperation(boids);
 		let wander = this.wander();
+		let edges = this.edges();
 
-		this.acceleration.add(alignment.mult(1.0));
-		this.acceleration.add(cohesion.mult(1.0));
+		this.acceleration.add(alignment.mult(1.1));
+		this.acceleration.add(cohesion.mult(1.1));
 		this.acceleration.add(seperation.mult(1.0));
 		this.acceleration.add(wander.mult(0.75));
+		this.acceleration.add(edges.mult(5.5));
 	}
 
 	update() {
@@ -117,18 +200,8 @@ class Boid {
 	}
 
 	wander() {
-		let wanderDistance = 10;
-    let wanderRadius = 24;
-    let wanderAngle = 0;
-    let wanderRange = 1;
-
-		// console.log(this.velocity.normalize())
 		let wanderCircle = createVector(this.position.x + this.velocity.copy().normalize().x * 24, this.position.y + this.velocity.copy().normalize().y * 24)
 		// circle(wanderCircle.x, wanderCircle.y, wanderRadius);
-    let wanderPoint = new createVector(wanderCircle.x, wanderCircle.y).rotate(wanderAngle);
-    let rand = p5.Vector.random2D();
-    // console.log(rand.normalize())
-
 		this.wanderAngle = (this.wanderAngle + (random() - 0.5) * 0.5) 
 		let x = cos(this.wanderAngle)*12;
 		let y = sin(this.wanderAngle)*12;
@@ -145,10 +218,6 @@ class Boid {
 
 	show() {
 		stroke(255);
-		if (this.id == 0) {
-			stroke(0, 255, 0);
-			// console.log(this.acceleration)
-		}
 
 		let triangleSize = 6;
 		strokeWeight(2);
@@ -172,21 +241,12 @@ class Boid {
 
 		stroke(255);
 
-		if(this.acceleration.x != 0 && this.acceleration.y != 0){
-			// line(
-			// 	this.position.x, this.position.y,
-			// 	this.position.x + this.acceleration.x * 100,
-			// 	this.position.y + this.acceleration.y * 100
-			// )
+		this.setMag
 
-			// line(
-			// 	this.position.x, this.position.y,
-			// 	this.position.x + this.velocity.x * 100,
-			// 	this.position.y + this.velocity.y * 100
-			// )
-
-			// console.log(this.velocity)
+		if(this.id == 0) {
+			line(this.position.x, this.position.y, (this.velocity.mag() * cos(this.velocity.heading()) * 25) + this.position.x, (this.velocity.mag() * sin(this.velocity.heading()) * 25) + this.position.y)
+			stroke(0, 0, 255);
+			line(this.position.x, this.position.y, (this.acceleration.mag() * cos(this.acceleration.heading()) * 250) + this.position.x, (this.acceleration.mag() * sin(this.acceleration.heading()) * 250) + this.position.y)
 		}
-		// circle(this.position.x, this.position.y, this.perception * 2)
 	}
 }
--- sketch.js
+++ sketch.js
@@ -1,21 +1,21 @@
 const flock = []
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(windowWidth, windowHeight-5);
 	for(let i=0; i<100; i++){
 	flock.push(new Boid(i))
 	}
 }
 
 function windowResized() {
-   resizeCanvas(windowWidth, windowHeight);
+   resizeCanvas(windowWidth, windowHeight-5);
 }
 
 function draw() {
 	background(51);
 
 	for(const boid of flock) {
-		boid.edges();
+		// boid.edges();
 		boid.flock(flock);
 		boid.update();
 		boid.show();
